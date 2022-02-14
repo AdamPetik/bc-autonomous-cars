e140e0ad206c65dbff4767673cba69ca0ffd1135
Adam Petík <petikadam@gmail.com>
2022-02-14 20:10:18 +0100

    fifo processing - updated test

--- NFTAutonomousVehicles/fifo_processing/test.py
+++ NFTAutonomousVehicles/fifo_processing/test.py
@@ -6,20 +6,30 @@ from NFTAutonomousVehicles.fifo_processing.processable import TaskCPUProcessable
 
 def _test():
     dt = 0.5
-    start_at = datetime(2020, 1, 1, 10, 50)
-    task = Task(instruction_count=150)
+    current_time = datetime(2020, 1, 1, 10, 50, 0)
+    start_at = datetime(2020, 1, 1, 10, 50, 0, int(0.6e6))
+    task1 = Task(instruction_count=150)
+    task2 = Task(instruction_count=150)
     cpu_processor = FIFOProcessor(100, dt)
-    task_processable = TaskCPUProcessable(task, start_at)
+    task_processable1 = TaskCPUProcessable(task1, start_at)
+    task_processable2 = TaskCPUProcessable(task2, start_at)
 
-    cpu_processor.add((start_at, task_processable))
+    cpu_processor.add((start_at,'1', task_processable1))
+    cpu_processor.add((start_at,'2', task_processable2))
 
-    processed = cpu_processor.process_all(start_at - timedelta(seconds=0.5))
-    assert(processed is None)
+    processed = cpu_processor.process_all(current_time)
+    assert(len(processed) == 0)
 
-    processed = cpu_processor.process_all(start_at)
-    assert(processed is None)
+    processed = cpu_processor.process_all(current_time + timedelta(seconds=0.5))
+    assert(len(processed) == 0)
 
-    processed = cpu_processor.process_all(start_at + timedelta(seconds=0.5))
+    processed = cpu_processor.process_all(current_time + timedelta(seconds=1))
+    print(processed[0].processed_at)
+
+    processed = cpu_processor.process_all(current_time + timedelta(seconds=1.5))
+    assert(len(processed) == 0)
+
+    processed = cpu_processor.process_all(current_time + timedelta(seconds=2))
     print(processed[0].processed_at)
 
 if __name__ == '__main__':
